aba51a1483df4b8b90f36422d143915dc1cc94c6
oracle_tenz <ngadesel@gmail.com>
2025-09-01 20:04:42 +0530

    first commit

---
+++ Day1/Tenzee.py
@@ -0,0 +1,66 @@
+a = 10
+b = 3
+print(a + b)	# Output: 13
+print(a - b)	# Output: 7
+print(a * b)	# Output: 30
+print(a / b)	# Output: 3.3333333333333335
+print(a % b)	# Output: 1 print(a ** b)	# Output: 1000 print(a // b)	# Output: 3
+x = 5
+y = 10
+print(x == y)	# Output: False print(x != y)	# Output: True print(x > y)		# Output: False
+print(x < y)	# Output: True
+print(x >= y)	# Output: False
+print(x <= y)	# Output: True
+a = True
+b = False
+print(a and b)	# Output: False
+print(a or b)	# Output: True
+print(not a)	# Output: False
+x = 5
+x += 2
+print(x)
+my_list = [1, 2, 3, 4, 5]
+print(3 in my_list)
+print(6 not in my_list)
+x = [1, 2, 3]
+y = [1, 2, 3]
+z = x
+print(x is z)	
+print(x is y)
+print(x is not y)
+x = 10
+y = 20
+result = x if x > y else y 
+print(result)
+a = 10
+b = 3
+print(a + b)	# Output: 13
+print(a - b)	# Output: 7
+print(a * b)	# Output: 30
+print(a / b)	# Output: 3.3333333333333335
+print(a % b)	# Output: 1 print(a ** b)	# Output: 1000 print(a // b)	# Output: 3
+x = 5
+y = 10
+print(x == y)	# Output: False print(x != y)	# Output: True print(x > y)		# Output: False
+print(x < y)	# Output: True
+print(x >= y)	# Output: False
+print(x <= y)	# Output: True
+a = True
+b = False
+print(a and b)	# Output: False
+print(a or b)	# Output: True
+print(not a)	# Output: False
+x = 5
+x += 2
+print(x)
+my_list = [1, 2, 3, 4, 5]
+print(3 in my_list)	# Output: True
+print(6 not in my_list)
+x = [1, 2, 3]
+y = [1, 2, 3]
+z = x
+print(x is z)	# Output: True print(x is y)	# Output: False print(x is not y)	# Output: True
+x = 10
+y = 20
+result = x if x > y else y 
+print(result)
